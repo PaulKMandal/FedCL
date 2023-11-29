6144c47fd0600dff6ac2e1e3243fbe92af1e7434
Paul Mandal <40133940+PaulKMandal@users.noreply.github.com>
2023-11-28 23:10:42 -0600

    Deleting code that shouldn't be in main.

--- src/tensor.c
+++ src/tensor.c
@@ -2,10 +2,6 @@
 #include "tensor.h"
 #include <stdio.h>
 
-/*Tensor create_tensor(size_t* dims, size_t n_dims, TensorType type, void*** data) {
-    // Implement create tensor
-}
-*/
 
 Tensor allocate_tensor(size_t* dims, TensorType type) {
     Tensor tensor;
@@ -39,17 +35,6 @@ Tensor allocate_tensor(size_t* dims, TensorType type) {
 
     return tensor;
 }
-
-/*Tensor reshape_tensor(Tensor* tensor, size_t new_dimensions) {
-    // Implement reshape tensor
-}
-*/
-
-/*
-Tensor permute_tensor(Tensor* tensor) {
-  // Implement reshape tensor
-}
-*/
 
 void free_tensor(Tensor* tensor) {
     for (size_t i = 0; i < tensor->num_dims; ++i) {
